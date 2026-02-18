658a4af1594659bf1ae7fb66e7786675c29e5b40
Sebastian Bogle <dev@sebastianbogle.com>
2026-02-18 20:20:07 +0000

    Implement async disposal pattern and enhance recipe state notification handling (#25)

--- Services/RecipeStateNotifier.cs
+++ Services/RecipeStateNotifier.cs
@@ -2,10 +2,32 @@ namespace Reci.Services;
 
 public class RecipeStateNotifier : IRecipeStateNotifier
 {
-    public event Action? OnRecipesChanged;
+    private readonly List<Func<Task>> _callbacks = new();
 
-    public void NotifyRecipesChanged()
+    public void Subscribe(Func<Task> callback)
     {
-        OnRecipesChanged?.Invoke();
+        ArgumentNullException.ThrowIfNull(callback);
+        _callbacks.Add(callback);
+    }
+
+    public void Unsubscribe(Func<Task> callback)
+    {
+        ArgumentNullException.ThrowIfNull(callback);
+        _callbacks.Remove(callback);
+    }
+
+    public async Task NotifyRecipesChangedAsync()
+    {
+        foreach (Func<Task> callback in _callbacks)
+        {
+            try
+            {
+                await callback();
+            }
+            catch (Exception)
+            {
+                // Individual handler failure should not prevent other handlers from executing.
+            }
+        }
     }
 }
