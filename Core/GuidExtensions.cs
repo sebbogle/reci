4fd89a9c6d6746b43ef9cc18376b9ffb0a7d2780
Sebastian Bogle <dev@sebastianbogle.com>
2026-02-20 19:33:50 +0000

    Ensure GUIDs are populated on import; remove old .reci files
    Added GuidExtensions with PopulateIfEmpty method to auto-generate GUIDs for recipes and groups during import. Updated DataTransferService to use this logic. Removed obsolete .reci files to support new import behavior.

---
+++ Core/GuidExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Reci.Core;
+
+public static class GuidExtensions
+{
+    public static Guid PopulateIfEmpty(this Guid guid)
+    {
+        return guid == Guid.Empty ? Guid.NewGuid() : guid;
+    }
+}
